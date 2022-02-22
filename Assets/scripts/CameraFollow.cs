1caf29a8d8bcf485667017c55efe987769bca8b8
Ken Rodenwaldt <kennyro@me.com>
2022-02-22 18:35:58 +0100

    add toggle in CameraFollow.cs, update 3D-Map, added post-processing

--- Assets/scripts/CameraFollow.cs
+++ Assets/scripts/CameraFollow.cs
@@ -1,23 +1,30 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraFollow : MonoBehaviour
 {
+    
+    public bool mouseControl;
     public Transform target;
 
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
 
+
     public float speedH = 2.0f;
     public float speedV = 2.0f;
-    private float yaw = 0.0f;
+    private float yaw = 90.0f;
     private float pitch = 0.0f;
 
     void Update()
     {
-    //     yaw += speedH * Input.GetAxis("Mouse X");
-    //     pitch += speedV * Input.GetAxis("Mouse Y");
+        if(mouseControl == true){
+        Cursor.visible = false;
+         yaw += speedH * Input.GetAxis("Mouse X");
+         pitch += speedV * -Input.GetAxis("Mouse Y");
+         transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        }
 
-    //     transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
     }
 
     void LateUpdate(){
