6df67b8fee60814d172eeedc7b233a17ba80b957
Ken Rodenwaldt <kennyro@me.com>
2022-04-13 12:23:17 +0200

    setup cameras

---
+++ Assets/scripts/CaptureScreenshot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaptureScreenshot : MonoBehaviour
+{
+
+    public int superSize = 2;
+    private int _shotIndex = 0;
+
+    private void Update () 
+    {
+        if(Input.GetKeyDown(KeyCode.A)) {
+            ScreenCapture.CaptureScreenshot($"aitrust-unityScreenShot{_shotIndex}.png", superSize);
+            Debug.Log("key pressed ");
+        }
+    }
+}
